cd83eb3142964e58f65f663d57bc17431ea1c669
Sturgos <sturgos@mdp.cc>
2018-12-02 22:41:52 +0100

    changing wp

--- install.sh
+++ install.sh
@@ -6,22 +6,22 @@ i3 \
 nm-applet \
 gnome-sound-applet \
 gnome-screensaver \
-redshift"
+redshift \
+fonts-powerline"
 
 # Utils
 APPS="${APPS} \
 vim \
-zsh \
-"
+zsh"
 
 # Lang
 APPS="${APPS} \
-python \
-python-pip \
-ruby \
 make \
 cmake \
 clang \
+python \
+python-pip \
+ruby \
 ruby"
 
 # TOOLS
@@ -35,19 +35,32 @@ most \
 volumeicon"
 
 # Install APPS
-# sudo apt update && sudo apt upgrade
-# sudo apt install $APPS -y 
+sudo apt update && sudo apt upgrade
+sudo apt install "$APPS" -y 
 
+## ZSH 
+# oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
 
 # Conf files
-# CONFDIR="~/.config"
-# if [ ! -d "$CONFDIR" ]; then
-# 	mkdir "$CONFDIR"
-# fi
-# cp .vimrc .zshrc .fehbg ~ 
-# cp i3/ $CONFDIR
-# cp redshift.conf $CONFDIR
-# sudo cp i3status.conf /etc/
+CONFDIR="~/.config"
+if [ ! -d "$CONFDIR" ]; then
+	mkdir "$CONFDIR"
+fi
+
+if [ ! -d "$CONFDIR/confIt" ]; then
+	mkdir "$CONFDIR/confIt"
+fi
+
+cp zsh_alliases zsh_functions "$CONFDIR/confIt"
+cp .vimrc .zshrc ~ 
+cp "i3/ $CONFDIR"
+cp redshift.conf "$CONFDIR/confIt"
+sudo cp i3status.conf /etc/
+
+# bg 
+cp .fehbg wp.jpg ~
 
 # gtk theme
 mkdir /tmp/confIT
@@ -55,6 +68,8 @@ cp themes/gtk/theme-obsidian-2-green.tar.gz /tmp/theme.tar.gz
 tar -xf /tmp/theme.tar.gz -C /tmp/confIT
 sudo cp -r /tmp/theme/theme-obsidian-2-green/Obsidian-2-green /usr/share/themes
 
-
 # clean
 rm -rf /tmp/confIT
+
+echo "you should change gpk theme: lxappearance
+and launch zsh."
